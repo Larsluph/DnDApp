71ba1ef7983500511c42b0e7382b7ce980c06f5d
Larsluph <remarso59@sfr.fr>
2022-10-22 00:11:29 +0200

    Add smart copy

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.WindowsAPICodePack.Dialogs;
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using System.Windows;
@@ -12,97 +13,164 @@ namespace DnDApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string? _smartCopySourceDir = null;
+        public string? SmartCopySourceDirectory
+        {
+            set
+            {
+                if (value is null)
+                {
+                    smartCopyToggle.IsChecked = false;
+                    smartCopySource.IsEnabled = false;
+                    _smartCopySourceDir = value;
+                }
+                else if (Directory.Exists(value))
+                {
+                    smartCopyToggle.IsChecked = true;
+                    smartCopySource.IsEnabled = true;
+                    _smartCopySourceDir = value;
+                }
+                UpdateTitle();
+            }
+        }
+
         private string _targetDir;
         public string TargetedDirectory
         {
-            get { return _targetDir; }
             set {
                 if (Directory.Exists(value))
                 {
                     _targetDir = value;
-                    root.Title = "DnDApp - " + _targetDir;
                 }
+                UpdateTitle();
             }
         }
-
-        public static RoutedUICommand toggle_topmost = new("Toggle Topmost", "toggle_topmost", typeof(MainWindow));
 
         public MainWindow()
         {
             InitializeComponent();
-            TargetedDirectory = NS_KnownFolders.KnownFolders.DownloadFolder;
+            _targetDir = NS_KnownFolders.KnownFolders.DownloadFolder;
+            UpdateTitle();
         }
 
         private void DragOverHandler(object sender, DragEventArgs e)
         {
-            e.Effects = (e.Data.GetDataPresent(DataFormats.FileDrop) ? DragDropEffects.Move : DragDropEffects.None);
+            e.Effects = e.Data.GetDataPresent(DataFormats.FileDrop) ?
+                e.KeyStates.HasFlag(DragDropKeyStates.ControlKey) ?
+                    DragDropEffects.Copy
+                    : DragDropEffects.Move
+                : DragDropEffects.None;
         }
 
         // An item just got dropped in the frame
-        private void DropHandler(object sender, DragEventArgs e)
+        private void DropHandler(object sender, DragEventArgs evt)
         {
             // get files from drop payload as FileDrop format
-            var payload = e.Data.GetData(DataFormats.FileDrop);
-
-            bool isCtrlPressed = Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl);
-            bool isShiftPressed = Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift);
-            bool isAltPressed = Keyboard.IsKeyDown(Key.LeftAlt) || Keyboard.IsKeyDown(Key.RightAlt);
+            var payload = evt.Data.GetData(DataFormats.FileDrop);
 
             // if payload isn't null, get selected paths as string array
             if (payload is string[] paths)
             {
                 foreach (string path in paths)
                 {
+                    string dest;
                     try
                     {
-                        if (Directory.Exists(path))
+                        // if smart copy is enabled
+                        if (_smartCopySourceDir is not null)
                         {
-                            // Move folder
-                            string dest = Path.Combine(TargetedDirectory, path.Split(Path.DirectorySeparatorChar).Last());
-                            if (isAltPressed) TargetedDirectory = path;
-                            else Directory.Move(path, dest);
-                        }
-                        else if (File.Exists(path))
-                        {
-                            // Move file
-                            string dest = Path.Combine(TargetedDirectory, Path.GetFileName(path));
-                            if (isCtrlPressed) File.Copy(path, dest, true);
-                            else File.Move(path, dest, true);
+                            // validate input file in source dir
+                            if (path.StartsWith(_smartCopySourceDir))
+                            {
+                                string relativePath = path[_smartCopySourceDir.Length..];
+                                dest = Path.Join(_targetDir, relativePath);
+                            }
+                            else
+                            {
+                                // Warn user that smartCopy can't occur
+                                MessageBox.Show("SmartCopy is enabled! You can't move files outside source folder when SmartCopy is enabled.",
+                                                "Invalid Operation",
+                                                MessageBoxButton.OK,
+                                                MessageBoxImage.Error);
+                                return;
+                            }
                         }
                         else
                         {
-                            // ???
-                            throw new InvalidDataException();
+                            
+                            if (Directory.Exists(path))
+                            {
+                                // Move folder
+                                dest = Path.Combine(_targetDir, path.Split(Path.DirectorySeparatorChar).Last());
+                            }
+                            else if (File.Exists(path))
+                            {
+                                // Move file
+                                dest = Path.Combine(_targetDir, Path.GetFileName(path));
+                            }
+                            else
+                            {
+                                // ???
+                                throw new InvalidOperationException();
+                            }
                         }
                     }
                     catch (Exception exception)
                     {
                         Console.WriteLine(exception.ToString());
+                        continue;
+                    }
+
+                    try
+                    {
+                        HandlePathOperation(path, dest, evt.KeyStates);
+                    } catch (Exception e)
+                    {
+                        MessageBox.Show(e.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                     }
                 }
             }
         }
 
+        private void HandlePathOperation(string path, string dest, DragDropKeyStates states)
+        {
+            bool isCtrlPressed = states.HasFlag(DragDropKeyStates.ControlKey);
+            //bool isShiftPressed = states.HasFlag(DragDropKeyStates.ShiftKey);
+            bool isAltPressed = states.HasFlag(DragDropKeyStates.AltKey);
+
+            if (Directory.Exists(path))
+            {
+                // Move folder
+                if (isAltPressed) TargetedDirectory = path;
+                //TODO: Handle copy
+                else Directory.Move(path, dest);
+            }
+            else if (File.Exists(path))
+            {
+                // Move file
+                if (_smartCopySourceDir is not null)
+                {
+                    string? destDirName = Path.GetDirectoryName(dest);
+                    if (destDirName is not null) Directory.CreateDirectory(destDirName);
+                }
+
+                if (isCtrlPressed) File.Copy(path, dest, true);
+                else File.Move(path, dest, true);
+            }
+            else
+            {
+                // ???
+                throw new InvalidOperationException();
+            }
+        }
+
         private void SelectTarget_Click(object sender, RoutedEventArgs e)
         {
-            var dlg = new CommonOpenFileDialog();
-            dlg.Title = "Select new DnD target";
-            dlg.IsFolderPicker = true;
-            dlg.InitialDirectory = _targetDir;
+            var result = OpenFolderPickerDialog("Select new DnD target", _targetDir);
 
-            dlg.AddToMostRecentlyUsedList = false;
-            dlg.AllowNonFileSystemItems = false;
-            dlg.DefaultDirectory = _targetDir;
-            dlg.EnsureFileExists = true;
-            dlg.EnsurePathExists = true;
-            dlg.EnsureReadOnly = false;
-            dlg.EnsureValidNames = true;
-            dlg.Multiselect = false;
-            dlg.ShowPlacesList = true;
-
-            if (dlg.ShowDialog() == CommonFileDialogResult.Ok)
+            if (result is not null)
             {
-                TargetedDirectory = dlg.FileName;
+                TargetedDirectory = result;
             }
         }
 
@@ -114,6 +182,63 @@ namespace DnDApp
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void ToggleSmartCopy_Click(object sender, RoutedEventArgs e)
+        {
+            if (_smartCopySourceDir is null)
+            {
+                var result = OpenFolderPickerDialog("Select new smart copy source", _targetDir);
+                if (result is not null)
+                {
+                    SmartCopySourceDirectory = result;
+                    return;
+                }
+            }
+
+            SmartCopySourceDirectory = null;
+        }
+
+        private void SelectSmartCopySource_Click(object sender, RoutedEventArgs e)
+        {
+            var result = OpenFolderPickerDialog("Select new smart copy source", _smartCopySourceDir);
+            if (result is not null)
+            {
+                SmartCopySourceDirectory = result;
+            }
+        }
+
+        private void UpdateTitle()
+        {
+            if (_smartCopySourceDir is not null)
+                root.Title = "DnDApp - " + _smartCopySourceDir + " -> " + _targetDir;
+            else
+                root.Title = "DnDApp - " + _targetDir;
+        }
+
+        public static string? OpenFolderPickerDialog(string title, string? initialDirectory)
+        {
+            var dlg = new CommonOpenFileDialog
+            {
+                Title = title,
+                IsFolderPicker = true,
+                AddToMostRecentlyUsedList = false,
+                AllowNonFileSystemItems = false,
+                EnsureFileExists = true,
+                EnsurePathExists = true,
+                EnsureReadOnly = false,
+                EnsureValidNames = true,
+                Multiselect = false,
+                ShowPlacesList = true
+            };
+
+            if (initialDirectory is not null)
+            {
+                dlg.InitialDirectory = initialDirectory;
+                dlg.DefaultDirectory = initialDirectory;
+            }
+
+            return (dlg.ShowDialog() == CommonFileDialogResult.Ok) ? dlg.FileName : null;
         }
     }
 }
